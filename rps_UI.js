917c6d657a6cd23a4535c883de4e09f3e7a27a56
Jean-François VERGNE <jeanfrancois.vergne@balyo.com>
2018-08-23 15:47:26 +0200

    V1.0

--- rps_UI.js
+++ rps_UI.js
@@ -1,9 +1,8 @@
 let playerPoints=0;
 let computerPoints=0;
-let nDraw=0;
 let nSet=1;
-let value=null;
-
+let playerSelection ="";
+let computerSelection ="";
 
 const displayPlayer= document.querySelector("#playerScore");
 const displayComputer= document.querySelector("#computerScore");
@@ -11,15 +10,14 @@ const msg = document.querySelector('#message');
 const pPoints = document.querySelector('#pPoints');
 const cPoints = document.querySelector('#cPoints');
 
-let playerSelection ="";
-let computerSelection ="";
+
 
 // That function will randomly return Rock, Paper or Scissor
 function computerPlay()
 {
     let answerNum = randomNumber(3);
     if(answerNum==0) return "rock";
-    if(answerNum==0) return "paper";
+    if(answerNum==1) return "paper";
     else return "scissors";
 }
 
@@ -30,84 +28,132 @@ function randomNumber(number){
 
 function playRound(playerSelection,computerSelection){
 
+    let winner = {};
+    winner.name="";
+    winner.points=0;
+
     if(playerSelection==computerSelection){
-        return "none";
+        winner.name="none";
+        nDraw+=1;
+        winner.points=nDraw;
+        return winner;
     }
 
     switch(playerSelection){
         case "rock":
             if(computerSelection == "paper"){
-                winner = "Computer";
+                winner.name = "computer";
+                computerPoints+=1;
+                winner.points = computerPoints;
             }else{
-                winner = "Player";
+                winner.name = "player";
+                playerPoints+=1;
+                winner.points = playerPoints;
             }
             break;
         case "paper":
             if(computerSelection == "scissors"){
-                winner = "Computer";
+                winner.name = "computer";
+                computerPoints+=1;
+                winner.points = computerPoints;
             }else{
-                winner = "Player";
+                winner.name = "player";
+                playerPoints+=1;
+                winner.points = playerPoints;
             }
             break;
         case"scissors":
-        default:
             if(computerSelection == "rock"){
-                winner = "Computer";
+                winner.name = "computer";
+                computerPoints+=1;
+                winner.points = computerPoints;
             }else{
-                winner = "Player";
+                winner.name = "player";
+                playerPoints+=1;
+                winner.points = playerPoints;
             }
+            break;
+            default: return "ERROR";
     }
-     return winner;
+
+    return winner;
 }
 
-function game(){
-
-
-    while (playerPoints<5 && computerPoints<5)
-    {
-        playerSelection=prompt("Round "+nGame+": What is your choice (rock, scissors or paper)?");
-
-        if (!checkValidity(playerSelection)){
-            continue;
-        }
-
-        computerSelection=computerPlay();
-
-        winner=playRound(playerSelection,computerSelection);
-
-        if(winner=="Player"){
-            playerPoints++;
-            console.log('Round '+ nGame +': Player Wins: ' +playerSelection+ ' beats '+computerSelection+' (Player : '+playerPoints+' - Computer : '+computerPoints+').');
-        }else if (winner=="Computer"){
-            computerPoints++;
-            console.log('Round '+ nGame +': Computer Wins: ' +computerSelection+ ' beats '+playerSelection+' (Player : '+playerPoints+' - Computer : '+computerPoints+').');
-        }else{
-            nDraw++;
-            console.log('Round '+ nGame +': No winner this time. Both players chose '+playerSelection+'. Try again. (Player : '+playerPoints+' - Computer : '+computerPoints+').');
-        }
-        nGame++;
+function displayScore(name){
+    if (name=="computer"){
+        cPoints.textContent = computerPoints;
     }
 
-    if(playerPoints==5)
-    {
-        console.log('Player WINS after '+ --nGame+' games and '+nDraw+' draws !!!');
-    }else {
-        console.log('Computer WINS after '+ --nGame+' games and '+nDraw+' draws !!!');
+    if (name=="player"){
+        pPoints.textContent = playerPoints;
     }
-
 }
 
-function checkValidity(playerSelection){
+function endOfGame(winner){
 
-    //making the entry cas insensitive
-    playerSelection=playerSelection.toLowerCase();
+    if(winner=="computer"){
+        msg.textContent="I WIN !!! Another game ?";
+    }else{
+        msg.textContent="I don't understand how it is possible but it seems that you won... Another game ?";
+    }
+    //diplsaying the start button again
+    start.style.cssText= 'display: inline-block';
+    displayComputer.style.display= "none";
+    displayPlayer.style.display= 'none';
+}
 
-    if (playerSelection!="paper" && playerSelection!="rock" && playerSelection!="scissors"){
-        console.log(playerSelection + " is not a valid choice... Try Again");
-        return 0;
+//When the user clicks on 1 of the buttons
+function choiceDone(){
+    console.log("Round #" + nSet + " has been played");
+
+    if(playerPoints==5){
+        msg.textContent="You already won, stop clicking on the button. Start another game!";
+        return;
     }
 
-    return 1;
+    if(computerPoints==5){
+        msg.textContent="Too late, you lost the game. Stop clicking on the buttons. Start another game.";
+        return;
+    }
+
+    //Getting the player seletion from the id of the button
+    playerSelection = this.getAttribute("id");
+    computerSelection = computerPlay();
+
+    winner=playRound(playerSelection,computerSelection);
+    console.log("Winner is "+ winner.name);
+    console.log(winner.name + " has "+ winner.points + " points");
+
+    displayScore(winner.name);
+
+    if(computerPoints==5 || playerPoints == 5){
+        endOfGame();
+    }
+
+    switch(winner.name){
+        case("computer"):
+            string="I win round #"+nSet+" ("+computerSelection+" beats "+playerSelection+")";
+        break;
+        case("player"):
+            string="You win round #"+nSet+" ("+playerSelection+" beats "+computerSelection+")";
+        break;
+            case("none"):
+            string="Round #"+nSet+" : is a draw! We both chose " + playerSelection;
+        break;
+        default: return "ERROR";
+    }
+
+    msg.textContent= string;
+    nSet+=1;
+}
+
+//Make the button a little larger
+function enlargeButton(){
+    this.classList.add('selection');
+}
+
+function downsizeButton(){
+    this.classList.remove('selection');
 }
 
 //resetting the variables for a new game
@@ -119,7 +165,7 @@ function reset(){
 
     pPoints.textContent=playerPoints;
     cPoints.textContent=computerPoints;
-    msg.textContent="Good luck!"
+    msg.textContent="Good luck!";
 }
 
 function startGame(){
@@ -128,11 +174,23 @@ function startGame(){
     start.style.cssText= 'display: none';
 
     //displaying the score buttons
-    displayComputer.style.display= 'block';
-    displayPlayer.style.display= 'block';
+    displayComputer.style.display= 'inline-block';
+    displayPlayer.style.display= 'inline-block';
 
     //resetting the values
     reset();
+
+    //Selecting all the buttons
+    const buttons= document.querySelectorAll("#choices div button");
+
+    console.log(buttons);
+
+    //adding listeners to those buttons
+    buttons.forEach(item => {
+        item.addEventListener('mouseover',enlargeButton);
+        item.addEventListener('click',choiceDone);
+        item.addEventListener('mouseleave',downsizeButton);
+    });
 }
 
 //starting the "game" function when a click is done on the element with id=start
